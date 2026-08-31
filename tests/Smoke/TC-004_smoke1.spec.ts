71eb3f230267bc756e96bf9e36038e8d3f7b5600
Manivel-S <42593986+Manivel-S@users.noreply.github.com>
2026-08-31 18:29:04 +0530

    Qadence publish: TC-004 smoke1 Commited By: sauceadmin

---
+++ tests/Smoke/TC-004_smoke1.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test';
+
+test(
+  'smoke1',
+  {
+    annotation: { type: 'QADENCE_TC_ID', description: 'TC-004' },
+    tag: ['@QADENCE_TC_ID:TC-004'],
+  },
+  async ({ page }) => {
+
+
+    await test.step('navigate', async () => {
+      await page.goto('https://www.saucedemo.com/');
+    });
+
+    await test.step('click', async () => {
+      await page.locator('[data-test="username"]').click();
+    });
+
+    await test.step('fill', async () => {
+      await page.locator('[data-test="username"]').fill('standard_user');
+    });
+
+    await test.step('click', async () => {
+      await page.locator('[data-test="password"]').click();
+    });
+
+    await test.step('fill', async () => {
+      await page.locator('[data-test="password"]').fill('secret_sauce');
+    });
+
+    await test.step('click', async () => {
+      await page.locator('[data-test="login-button"]').click();
+    });
+
+    await test.step('click', async () => {
+      await page.locator("xpath=/html/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]").click();
+    });
+
+  }
+);
+
