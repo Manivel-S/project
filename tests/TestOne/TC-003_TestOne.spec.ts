d4a54f5c8e3a95ed5c43c4c3e2e8281dfcbcd090
Manivel-S <42593986+Manivel-S@users.noreply.github.com>
2026-08-31 15:52:25 +0530

    Qadence edit: TC-003 Test Updated By: sauceadmin

--- tests/TestOne/TC-003_TestOne.spec.ts
+++ tests/TestOne/TC-003_TestOne.spec.ts
@@ -25,8 +25,12 @@ test(
       await page.locator('[data-test="password"]').click();
     });
 
+//     await test.step('fill', async () => {
+//       await page.locator('[data-test="password"]').fill('secret_sauce');
+//     });
+
     await test.step('fill', async () => {
-      await page.locator('[data-test="password"]').fill('secret_sauce');
+      await page.locator('[data-test="passport"]').fill('secret_sauce');
     });
 
     await test.step('click', async () => {
