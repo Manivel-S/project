4716cbf55b34e12f36cd293d9cb1a2ee8108d9d5
Manivel-S <42593986+Manivel-S@users.noreply.github.com>
2026-09-24 19:10:19 +0530

    Qadence publish: TC-023 draft5 Commited By: sauceadmin

---
+++ tests/Wrong/TC-023_draft5.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test(
+  'draft5',
+  {
+    annotation: { type: 'QADENCE_TC_ID', description: 'TC-023' },
+    tag: ['@QADENCE_TC_ID:TC-023'],
+  },
+  async ({ page }) => {
+
+
+    await test.step('#01 - navigate', async () => {
+      await page.goto('https://www.saucedemo.com/');
+    });
+
+    await test.step('#02 - click', async () => {
+      await page.locator('[data-test="login-credentials-container"]').locator('[data-test="login-password"]').click();
+    });
+
+    await test.step('#03 - click', async () => {
+      await page.locator('[data-test="login-credentials-container"]').locator('[data-test="login-credentials"]').click();
+    });
+
+    await test.step('#04 - click', async () => {
+      await page.getByRole("textbox", { name: "Username", exact: true }).click();
+    });
+
+    await test.step('#05 - click', async () => {
+      await page.getByRole("textbox", { name: "Password", exact: true }).click();
+    });
+
+  }
+);
+
