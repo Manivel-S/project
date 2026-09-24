b496d321391fef279cd5722bef88af254f72c324
Manivel-S <42593986+Manivel-S@users.noreply.github.com>
2026-09-24 19:10:22 +0530

    Qadence publish: TC-022 draft4 Commited By: sauceadmin

---
+++ tests/Wrong/TC-022_draft4.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+
+test(
+  'draft4',
+  {
+    annotation: { type: 'QADENCE_TC_ID', description: 'TC-022' },
+    tag: ['@QADENCE_TC_ID:TC-022'],
+  },
+  async ({ page }) => {
+
+
+    await test.step('#01 - navigate', async () => {
+      await page.goto('https://www.saucedemo.com/');
+    });
+
+    await test.step('#02 - click', async () => {
+      await page.locator('[data-test="login-credentials-container"]').locator('[data-test="login-credentials"]').click();
+    });
+
+    await test.step('#03 - click', async () => {
+      await page.locator('[data-test="login-credentials-container"]').locator('[data-test="login-password"]').click();
+    });
+
+    await test.step('#04 - click', async () => {
+      await page.getByRole("main").filter({ hasText: "LoginAccepted usernames" }).click();
+    });
+
+    await test.step('#05 - click', async () => {
+      await page.getByRole("textbox", { name: "Username", exact: true }).click();
+    });
+
+    await test.step('#06 - click', async () => {
+      await page.getByRole("textbox", { name: "Password", exact: true }).click();
+    });
+
+  }
+);
+
