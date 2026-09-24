eabd0ccc7b7dc5b337fda6961a5afc98d4988330
Manivel-S <42593986+Manivel-S@users.noreply.github.com>
2026-09-24 18:58:36 +0530

    Qadence publish: TC-016 GitCheckName Commited By: sauceadmin

---
+++ tests/Smoke/TC-016_GitCheckName.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test(
+  'GitCheckName',
+  {
+    annotation: { type: 'QADENCE_TC_ID', description: 'TC-016' },
+    tag: ['@QADENCE_TC_ID:TC-016'],
+  },
+  async ({ page }) => {
+
+
+    await test.step('#01 - navigate', async () => {
+      await page.goto('https://www.saucedemo.com/');
+    });
+
+    await test.step('#02 - click', async () => {
+      await page.getByRole("main").filter({ hasText: "LoginAccepted usernames" }).click();
+    });
+
+    await test.step('#03 - click', async () => {
+      await page.locator('[data-test="login-credentials-container"]').locator('[data-test="login-credentials"]').click();
+    });
+
+    await test.step('#04 - click', async () => {
+      await page.locator('[data-test="login-credentials-container"]').locator('[data-test="login-password"]').click();
+    });
+
+    await test.step('#05 - click', async () => {
+      await page.locator('[data-test="login-credentials-container"]').locator('[data-test="login-credentials"]').click();
+    });
+
+  }
+);
+
