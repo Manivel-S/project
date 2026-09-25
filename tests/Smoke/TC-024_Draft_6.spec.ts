3107b7faf2f8e0eb98df12e938e05d7a48a601bb
Manivel-S <42593986+Manivel-S@users.noreply.github.com>
2026-09-25 13:56:46 +0530

    Qadence edit: TC-024 Draft 6 Updated By: null

--- tests/Smoke/TC-024_Draft_6.spec.ts
+++ tests/Smoke/TC-024_Draft_6.spec.ts
@@ -29,10 +29,6 @@ test(
       await page.locator('[data-test="login-credentials-container"]').locator('[data-test="login-password"]').click();
     });
 
-    await test.step('#06 - click', async () => {
-      await page.locator('[data-test="login-container"]').locator('[data-test="login-credentials-container"]').click();
-    });
-
   }
 );
 
